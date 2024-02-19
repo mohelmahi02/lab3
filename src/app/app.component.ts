2a4db9940d4064c0344432643cc30e3b73266bb6
unknown <g00407950@atu.ie>
2024-02-19 10:25:27 +0000

    lab3 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AComponent,BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title:string = 'my-first-app';
+  name:string = "Martin";
+  age:number = 22;
 }
